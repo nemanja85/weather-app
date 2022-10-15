660cd4bb7982598a7abb086957495145ce629e24
nemanja85 <mitic@deutsche-teilkauf.de>
2022-10-15 14:14:40 +0200

    Define container and the breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,17 @@ module.exports = {
     fontFamily: {
       Roboto: ['Roboto, sans-serif'],
     },
+    container: {
+      padding: '1.5rem',
+      center: true,
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       spacing: {
         '8xl': '96rem',
