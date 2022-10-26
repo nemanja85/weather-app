436a5e1dbec5722ff4092db86bf3c0244aea9186
nemanja85 <mitic@deutsche-teilkauf.de>
2022-10-26 21:31:33 +0200

    Added search input field

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <script setup></script>
 
 <template>
-  <main></main>
+  <main class="container mx-auto text-gray-100">
+    <div class="relative py-6">
+      <input
+        type="text"
+        placeholder="Search for a city or state"
+        class="w-full p-2 bg-transparent border-b focus:outline-none focus:shadow-md"
+      />
+    </div>
+  </main>
 </template>
