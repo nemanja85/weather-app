0c69877f5cfab2989e4502f850085d0dec7e7b20
nemanja85 <mitic@deutsche-teilkauf.de>
2022-11-02 22:21:40 +0100

    Added calculation for time zone

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -6,12 +6,26 @@ const route = useRoute();
 const getWeatherData = async () => {
   try {
     const weatherData = await axios.get(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid={API key}`
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid= 4dbbb5fbef639a52415edc87fa9ef5a7&units=imperial`
     );
+
+    const localOffset = new Date().getTimezoneOffset() * 60000;
+    const utc = weatherData.data.current.dt * 1000 + localOffset;
+    weatherData.data.currentTime = utc + 1000 * weatherData.data.timezone_offset;
+
+    weatherData.data.hourly.forEach((hour) => {
+      const utc = hour.dt * 1000 + localOffset;
+      hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
+    });
+
+    return weatherData;
   } catch (err) {
     console.error(err);
   }
 };
+
+const weatherData = await getWeatherData();
+console.log(weatherData);
 </script>
 
 <template>
