5ceb0228c7f6aba022b3d7921186d26c2408b675
nemanja85 <mitic@deutsche-teilkauf.de>
2022-11-02 22:04:21 +0100

    Connect with OpenWeather API

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const getWeatherData = async () => {
+  try {
+    const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid={API key}`
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+</script>
+
+<template>
+  <div></div>
+</template>
