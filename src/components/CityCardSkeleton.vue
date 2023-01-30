212b697ec83940dc52f172f78d9f1cfad272d3c2
nemanja85 <mitic@deutsche-teilkauf.de>
2023-01-30 23:05:08 +0100

    Added animated skeleton on city list

---
+++ src/components/CityCardSkeleton.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AnimatedPlaceholder from './AnimatedPlaceholder.vue';
+</script>
+<template>
+  <div class="flex px-3 py-6 bg-gray-800 rounded-sm shadow-md">
+    <div class="flex flex-col flex-1 gap-2">
+      <AnimatedPlaceholder class="max-w-[50%]" />
+      <AnimatedPlaceholder class="max-w-[40%]" />
+    </div>
+
+    <div class="flex items-end flex-1 gap-2">
+      <AnimatedPlaceholder class="w-full max-w-[50px]" />
+      <AnimatedPlaceholder class="w-full max-w-[40px]" />
+    </div>
+  </div>
+</template>
