f76565d4a35f02c1296bbc5bef3568a20b0a5dc0
nemanja85 <mitic@deutsche-teilkauf.de>
2023-02-12 19:59:45 +0100

    Added skeleton on city view

---
+++ src/components/CityViewSkeleton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import AnimatedPlaceholder from './AnimatedPlaceholder.vue';
+</script>
+<template>
+  <div class="flex flex-col flex-1">
+    <!-- Overview -->
+    <div class="flex flex-col items-center py-12">
+      <AnimatedPlaceholder class="max-w-[300px] w-full mb-2" />
+      <AnimatedPlaceholder class="max-w-[300px] w-full mb-12" />
+      <AnimatedPlaceholder class="max-w-[300px] h-[100px] w-full mb-12" />
+      <AnimatedPlaceholder class="max-w-[300px] w-full mb-8" />
+      <AnimatedPlaceholder class="max-w-[300px] h-[75px] w-full" />
+      <AnimatedPlaceholder class="max-w-[300px] w-full mb-8" />
+      <AnimatedPlaceholder class="max-w-[300px] w-full mb-8" />
+      <AnimatedPlaceholder class="max-w-[300px] w-full mb-8" />
+      <AnimatedPlaceholder class="max-w-[300px] w-full mb-8" />
+    </div>
+  </div>
+</template>
